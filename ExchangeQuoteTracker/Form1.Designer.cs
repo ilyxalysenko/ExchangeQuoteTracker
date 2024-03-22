982832a3ba47438fb6cc7561b675890067dff6a7
Лисёнок Живой <bleach-2001@mail.ru>
2024-03-22 05:58:51 +0300

    next design UI

--- ExchangeQuoteTracker/Form1.Designer.cs
+++ ExchangeQuoteTracker/Form1.Designer.cs
@@ -47,7 +47,6 @@ namespace ExchangeQuoteTracker
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.listView1 = new System.Windows.Forms.ListView();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // timer
@@ -60,9 +59,9 @@ namespace ExchangeQuoteTracker
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(-1, 0);
+            this.listView1.Location = new System.Drawing.Point(-1, 26);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(802, 404);
+            this.listView1.Size = new System.Drawing.Size(328, 424);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
@@ -78,30 +77,18 @@ namespace ExchangeQuoteTracker
             "ETHUSD",
             "ETHUSDC",
             "POLYXKRW",
-            "PEPEUSDT",
-            
-            });
-            this.comboBox1.Location = new System.Drawing.Point(13, 410);
+            "PEPEUSDT"});
+            this.comboBox1.Location = new System.Drawing.Point(-1, -2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(328, 28);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(347, 410);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(328, 28);
-            this.comboBox2.TabIndex = 2;
-            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox2);
+            this.ClientSize = new System.Drawing.Size(325, 450);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listView1);
             this.Name = "Form1";
@@ -119,7 +106,6 @@ namespace ExchangeQuoteTracker
 
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
